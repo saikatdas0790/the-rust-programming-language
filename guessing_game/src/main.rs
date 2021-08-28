a6d92ce7aed1262b3ed20a47935a116444218a92
Saikat Das <saikatdas0790@gmail.com>
2021-08-28 18:29:39 +0530

    chore: complete guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the nubmer!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
@@ -11,6 +11,7 @@ fn main() {
         println!("Please input your guess.");
 
         let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
@@ -19,7 +20,9 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
         println!("You guessed: {}", guess);
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
