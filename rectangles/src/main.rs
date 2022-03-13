7c2746e46cc026419669df699b9465f916a33ab1
Saikat Das <saikatdas0790@gmail.com>
2022-03-13 16:23:41 +0530

    5.3 - method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,22 +3,31 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-
-    println!("rect1 is {:?}", rect1);
-    dbg!(&rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
