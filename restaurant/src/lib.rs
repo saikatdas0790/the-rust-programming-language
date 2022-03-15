512e6ffae6bd4b8a72192110d934edaffd52b30b
Saikat Das <saikatdas0790@gmail.com>
2022-03-15 21:00:46 +0530

    7.4 - bringing paths into scope with the use keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -39,12 +39,18 @@ mod back_of_house {
     }
 }
 
+use crate::front_of_house::hosting::add_to_waitlist;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    add_to_waitlist();
+    add_to_waitlist();
+    add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
