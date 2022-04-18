b7812548ca6473bf9cd99e021a18b7f040f470e3
Saikat Das <saikatdas0790@gmail.com>
2022-04-18 21:42:46 +0530

    13.3 - improving our i/o project

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,10 +2,8 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
