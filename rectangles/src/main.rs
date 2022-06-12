5b8eeb13b71aa2786482f98503df5f84c0ffd6bf
Saikat Das <saikatdas0790@gmail.com>
2022-06-12 16:51:14 +0530

    5.3 - method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -26,8 +26,24 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    #[allow(unused_variables)]
+    let sq = Rectangle::square(3);
 }
