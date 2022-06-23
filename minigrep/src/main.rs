78a1ea4264fe2b47803ee48bf51599ade67604fb
Saikat Das <saikatdas0790@gmail.com>
2022-06-23 19:52:05 +0530

    12.1 - accepting command line arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,10 @@
-use minigrep::Config;
-use std::{env, process};
-
+use std::env;
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let args: Vec<String> = env::args().collect();
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {}", e);
+    let query = &args[1];
+    let filename = &args[2];
 
-        process::exit(1);
-    }
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
